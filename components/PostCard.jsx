9f3e0ba802cb4a188f985b2e86eeaa4779fc1c57
Mugdha <mugdharbehere@gmail.com>
2025-04-12 11:29:30 +0530

    modified

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -26,15 +26,15 @@ const PostCard = ({ post }) => (
     </h1>
     <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
       <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
-        <Image
-          unoptimized
-          loader={grpahCMSImageLoader}
-          alt={post.author.name}
-          height="30px"
-          width="30px"
-          className="align-middle rounded-full"
-          src={post.author.photo.url}
-        />
+      <Image
+  unoptimized
+  loader={grpahCMSImageLoader}
+  alt={post.author.name}
+  width={30}
+  height={30}
+  className="rounded-full"
+/>
+
         <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">{post.author.name}</p>
       </div>
       <div className="font-medium text-gray-700">
