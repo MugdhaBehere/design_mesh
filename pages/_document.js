b47b55b131accafef9b3f176cdf6a8597611040a
Mugdha <mugdharbehere@gmail.com>
2025-04-12 11:25:40 +0530

    modified

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -19,7 +20,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-        
+   
         </body>
       </Html>
     );
