a2c61602a377e640d1b4f963a643e71355dc0bca
Mugdha <mugdharbehere@gmail.com>
2025-04-13 15:28:22 +0530

    modified

--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -8,15 +8,15 @@ import { getFeaturedPosts } from '../services';
 const responsive = {
   superLargeDesktop: {
     breakpoint: { max: 4000, min: 1024 },
-    items: 5,
+    items: 3,
   },
   desktop: {
     breakpoint: { max: 1024, min: 768 },
-    items: 3,
+    items: 2,
   },
   tablet: {
     breakpoint: { max: 768, min: 640 },
-    items: 2,
+    items: 1,
   },
   mobile: {
     breakpoint: { max: 640, min: 0 },
@@ -36,16 +36,16 @@ const FeaturedPosts = () => {
   }, []);
 
   const customLeftArrow = (
-    <div className="absolute arrow-btn left-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <div className="absolute arrow-btn left-4 text-center py-3 cursor-pointer bg-pink-600 rounded-full z-40">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
       </svg>
     </div>
   );
 
   const customRightArrow = (
-    <div className="absolute arrow-btn right-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <div className="absolute arrow-btn right-4 text-center py-3 cursor-pointer bg-pink-600 rounded-full z-40">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
       </svg>
     </div>
@@ -53,10 +53,20 @@ const FeaturedPosts = () => {
 
   return (
     <div className="mb-8">
-      <Carousel infinite customLeftArrow={customLeftArrow} customRightArrow={customRightArrow} responsive={responsive} itemClass="px-4">
-        {dataLoaded && featuredPosts.map((post, index) => (
-          <FeaturedPostCard key={index} post={post} />
-        ))}
+      <Carousel
+        infinite
+        responsive={responsive}
+        customLeftArrow={customLeftArrow}
+        customRightArrow={customRightArrow}
+        itemClass="px-6"
+        containerClass="pb-4"
+      >
+        {dataLoaded &&
+          featuredPosts.map((post, index) => (
+            <div key={index} className="h-full">
+              <FeaturedPostCard post={post} />
+            </div>
+          ))}
       </Carousel>
     </div>
   );
