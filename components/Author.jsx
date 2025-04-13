d19a39acfc7237c709af4fb72d74c47804a65f68
Mugdha <mugdharbehere@gmail.com>
2025-04-13 15:26:10 +0530

    modified

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import Image from 'next/image';
-
 import { grpahCMSImageLoader } from '../util';
 
 const Author = ({ author }) => (
-  <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
-    <div className="absolute left-0 right-0 -top-14">
-    <Image
+  <div className="text-center mt-16 mb-8 p-12 rounded-lg bg-black bg-opacity-20">
+    {/* Safe-centered Author Image */}
+    <div className="flex justify-center mb-4">
+      <Image
         unoptimized
         loader={grpahCMSImageLoader}
         alt={author.name}
         height={60}
         width={60}
-        className="align-middle rounded-full object-cover shadow-lg"
+        className="rounded-full object-cover shadow-lg"
         src={author.photo.url}
       />
     </div>
-    <h3 className="text-white mt-4 mb-4 text-xl font-bold">{author.name}</h3>
+    <h3 className="text-white mb-4 text-xl font-bold">{author.name}</h3>
     <p className="text-white text-ls">{author.bio}</p>
   </div>
 );
