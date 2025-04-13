082052b3e3068ca4bb64280b2e74f33923adaddc
Mugdha <mugdharbehere@gmail.com>
2025-04-13 15:20:59 +0530

    modified

--- components/Author.jsx
+++ components/Author.jsx
@@ -5,9 +5,8 @@ import { grpahCMSImageLoader } from '../util';
 
 const Author = ({ author }) => (
   <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
-    {/* Centered Author Image */}
-    <div className="absolute left-1/2 -top-14 transform -translate-x-1/2">
-      <Image
+    <div className="absolute left-0 right-0 -top-14">
+    <Image
         unoptimized
         loader={grpahCMSImageLoader}
         alt={author.name}
@@ -17,7 +16,7 @@ const Author = ({ author }) => (
         src={author.photo.url}
       />
     </div>
-    <h3 className="text-white mt-8 mb-4 text-xl font-bold">{author.name}</h3>
+    <h3 className="text-white mt-4 mb-4 text-xl font-bold">{author.name}</h3>
     <p className="text-white text-ls">{author.bio}</p>
   </div>
 );
